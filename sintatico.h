124fa825e6e3079848095d19413a06dfc87fb1d1
MCR2ZY <mauriciosm95@gmail.com>
2017-11-08 11:13:09 -0300

    Adiciona as definições do compilador sintático.

--- sintatico.h
+++ sintatico.h
@@ -1,6 +1,48 @@
+/**
+******************************************************************************
+* @file	sintatico.h
+* @author Marcelo Henrique, Mauricio Souza
+* @version  x.x 
+* @date 08.11.17
+* @brief Cabeçalho do Analisador Sintático
+******************************************************************************
+*/
 #ifndef SINTATICO_H_INCLUDED
 #define SINTATICO_H_INCLUDED
 
+#include "lexico.h"
+
+#define QntSimbolos 100
+
+typedef enum tipoSimbolo{
+    BOOLEANO, CARACTER, INTEIRO, REAL, SEMRETORNO        
+} TipoSimbolo;
+
+typedef enum categoriaSimbolo{
+    FUNCAO, PARAMETRO, VARIAVEL
+} CategoriaSimbolo;
+
+typedef enum escopoSimbolo{
+    GLOBAL, LOCAL
+} EscopoSimbolo;
+
+
+typedef struct simbolo{
+    char lexema[TamLexema];
+    TipoSimbolo tipo;
+    CategoriaSimbolo categoria;
+    EscopoSimbolo escopo;
+} Simbolo;
+
+void prog();
+void decl();
+void tipo();
+void func();
+void cmd();
+void atrib();
+void expr();
+
+extern Simbolo TabelaSimbolos[QntSimbolos];
 
 
 #endif // SINTATICO_H_INCLUDED
