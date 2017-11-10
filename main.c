4372723bac5febc5be3ec9a6ea341c270a5c86ac
MCR2ZY <mauriciosm95@gmail.com>
2017-11-10 13:20:17 -0300

    Altera biblioteca principal para "sintatico.c". Remove função temporaria para impressão dos tokens.

--- main.c
+++ main.c
@@ -7,12 +7,11 @@
 * @brief
 ******************************************************************************
 */
-#include "analex.h"
+#include "sintatico.c"
 
 Token token;
 FILE *fp;
 
-void imprimeToken(Token ); //Função temporaria de teste para imprimir tokens
 
 int main(int argc, char const *argv[])
 {
@@ -28,58 +27,9 @@ int main(int argc, char const *argv[])
         exit(1);
     }
 
-    while(!feof (fp)){
-        analex(fp);
-        imprimeToken(token);
-    }
-
+    //analex(fp);
+    sintatico();
+    printf("\n\nFECHANDO ARQUIVO...\n");
     fclose(fp);
     return 0;
-}
-
-void imprimeToken(Token tkn){
-
-    switch (tkn.tipo){
-        case CT_C:
-            printf("\n\t< CT_C, \" %s \" >", tkn.lexema);;
-            break;
-        case CT_I:
-            printf("\n\t< CT_I, \" %d \" >", tkn.valor.valorInt);
-            break;
-        case CT_L:
-            printf("\n\t< CT_L, \" %s \" >", tkn.lexema);
-            break;
-        case CT_R:
-            printf("\n\t< CT_R, \" %f \" >", tkn.valor.valorReal);
-            break;
-        case ID:
-            printf("\n\t< ID, \" %s \" >", tkn.lexema);
-            break;
-        case PR:
-            printf("\n\t< PR, \" %s \" >", TabPalReservadas[tkn.valor.codPR]);
-            break;
-        case SN:
-            printf("\n\t< SN, \" %s \" >", tkn.lexema);
-            break;
-        case END:
-            if(tkn.valor.valorInt == 1){
-            printf("\n\t< END, \"ERRO\" >");
-            printf ("\n\n\tAnalise lexica executada com sucesso.\n");
-            printf("\n\n\tEncerando...\n");
-            exit (1);
-            }
-            else{
-                printf("\n\t< END, \"FIM\" >");
-                printf ("\n\n\tAnalise lexica executada com sucesso.\n");
-                printf("\n\n\tEncerando...\n");
-            }
-            break;
-        default:
-            printf("\n\t< END, \"DEFAULT\" >");
-            printf ("\n\n\tAnalise lexica executada com sucesso.\n");
-            printf("\n\n\tEncerando...\n");
-            break;
-    }
-
-    return;
 }
